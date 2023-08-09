bc7583f54965ea84d90e2669d580af1cdeb40ace
santlunaschiavi <santlunaschiavi@gmail.com>
2023-08-08 21:58:13 -0300

    primer commit

---
+++ src/componente/cartel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './cartel.css'
+
+export default function Cartel(){
+    return (
+        <div className="cartelcontainer">
+        Santiago Luna
+        </div>
+    )
+}
